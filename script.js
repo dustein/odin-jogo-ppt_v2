4de06a2968937540639a5eed019aa7f36e79e138
dustein <steindu@gmail.com>
2022-07-14 16:48:40 -0300

    travado

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 
+//jogada computador
+let choices = ["pedra", "papel", "tesoura"]
+computerPlay = choices[Math.floor(Math.random()*3)]
+console.log(`COMPUTADOR escolheu ${computerPlay}.`)
 
-let playerSelection = ''
+//jogado do user
 
-//jogada do user pelo botao
-  const botoes = document.querySelectorAll('.botoes');
-  const jogadaDiv = document.querySelector('#jogada');
-  botoes.forEach(botoes => {
-    botoes.addEventListener('click', () => {
-      playerSelection = botoes.id
-      // console.log(playerSelection)
-      jogada.innerText = playerSelection
-      return "oi"
-    })
+const exibeJogada = document.querySelector('#jogada');
+
+const userPlay = () => {
+  const botoes = document.querySelectorAll('#botoes')
+  
+  botoes.forEach(botao => {
+    botao.addEventListener('click', () => { return botao.id})
   })
+}
 
-  //
+function gameOn(computerPlay, userPlay) {
+  
+  return `computador ${computerPlay}, user ${userPlay}.`
+}
 
-
-
+console.log(gameOn(computerPlay, userPlay))

9f60edc0bb762f04b1cb6f624d1aa3481cbf3175
DuStein <steindu@gmail.com>
2022-07-24 15:46:45 -0300

    botoes e logica funcionando

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //area de axibicao da jogada
 const exibeJogada = document.querySelector('#jogada');
+const exibeRodada = document.querySelector('#rodada')
 //fim exibicao jogada
 
 //jogada computador
@@ -48,6 +49,7 @@ function gameRules(user, pc) {
       } else {
         console.log('caso não previsto')
       }
+    console.log("ponto pra " + point)
     return point
 }
 
@@ -55,57 +57,32 @@ function gameRules(user, pc) {
 function rodada() {
   let user;
   let pc;
+  let pontoUser = 0;
+  let pontoPc = 0;
+  let pontoEmpate = 0;
   const botoes = document.querySelectorAll('.botoes');
   botoes.forEach(botao => {
     botao.addEventListener('click', escolha => {
       user = escolha.target.id
       pc = computerPlay();
-      console.log(`User escolheu ${user}`)
-      console.log(`Computador esconlheu ${pc}`)
+      console.log(`User escolheu ${user}. Computador escolheu ${pc}`)
 
-      gameRules(user, pc)
-      console.log(gameRules(user, pc))
+      vencedor = gameRules(user, pc)
+      if (vencedor === 0) {
+        console.log("ponto pro COMPUTADOR!")
+        exibeRodada.innerText = "... Ponto pro compuador"
+        pontoPc += 1;
+      } else if (vencedor === 1) {
+        console.log("ponto pro USER")
+        exibeRodada.innerText = "... Ponto pro User"
 
-
-
-
-      //logica do jogo
-      //pedra x papel
-      // if(user === 'pedra' && pc === 'papel') {
-      //   exibeJogada.innerText = 'Papel enrola Pedra, você perdeu'
-      //   console.log('papel enrola pedra, user perdeu')
-      // } else if (user === 'papel' && pc === 'pedra') {
-      //   exibeJogada.innerText = 'Papel enrola Pedra, você VENCEU'
-      //   console.log('papel enrola pedra, user VENCEU')
-      //   point = 1
-      // }
-      // //pedra x tesoura
-      // else if (user === 'pedra' && pc === 'tesoura') {
-      //   exibeJogada.innerText = 'Pedra quebra Tesoura, você VENCEU'
-      //   console.log('pedra quebra tesoura, user VENCEU')
-      //   point = 1
-      // } else if (user === 'tesoura' && pc === 'pedra') {
-      //   exibeJogada.innerText = 'Pedra quebra Tesoura, você perdeu'
-      //   console.log('pedra quebra tesoura, user perdeu')
-      // }
-      // //tesoura x papel
-      // else if (user === 'tesoura' && pc === 'papel') {
-      //   exibeJogada.innerText = 'Tesoura corta Papel, user VENCEU'
-      //   console.log('tesoura corta papel, user VENCEU')
-      //   point = 1
-      // } else if (user === 'papel' && pc === 'tesoura') {
-      //   exibeJogada.innerText = 'Tesoura corta Papel, user perdeu'
-      //   console.log('tesoura corta papel, user perdeu')
-      // }
-      // // empate
-      // else if (user === pc) {
-      //   exibeJogada.innerText = 'Jogaram igual, empate na rodada'
-
-      //   console.log('jogaram igual, empate na rodada')
-      //   point = 2
-      // } else {
-      //   console.log('caso não previsto')
-      // }
+        pontoUser += 1;
+      } else {
+        console.log("EMPATE!")
+        pontoEmpate += 1;
+      }
+      
+      console.log(`pontos user ${pontoUser} | empates ${pontoEmpate} | pontos computador ${pontoPc}`)
     })
   })
   return 'rodou rodada'
@@ -113,8 +90,7 @@ function rodada() {
 
 function game() {
   rodada();
-  console.log('rodada,,,,')
-
+  return "ok rodou uma"
 }
 
 console.log(game())
